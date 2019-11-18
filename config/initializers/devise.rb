1753d5e07cf14e039c661bf71e4b3e045ed31776
Evgeniy Krokhmal <ekrokhmal@cleverua.com>
2019-11-19 01:34:36 +0200

    changed config.omniauth credentials - redirect url

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "555303038633835", "0b58d00641e24b866f7a63335114d122", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, "555303038633835", "0b58d00641e24b866f7a63335114d122", callback_url: "https://safe-stream-76259.herokuapp.com/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
