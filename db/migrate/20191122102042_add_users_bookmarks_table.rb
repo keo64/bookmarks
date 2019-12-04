c0ddcf209326dd63d7fc934d55a81dbf19a2387d
Evgeniy Krokhmal <ekrokhmal@cleverua.com>
2019-12-04 13:31:15 +0200

    delete add users bookmarks migration

--- db/migrate/20191122102042_add_users_bookmarks_table.rb
+++
@@ -1,8 +0,0 @@
-class AddUsersBookmarksTable < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users_bookmarks, id: false do |t|
-      t.belongs_to :user
-      t.belongs_to :bookmark
-    end
-  end
-end
